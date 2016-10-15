c116c831ba831594f906545e36d875ef6f33c7af
Eric Betts <bettse@fastmail.fm>
2016-10-15 14:10:44 -0700

    Hellow world example sketch

--- src/main.ino
+++ src/main.ino
@@ -1,19 +1,51 @@
 /*
-   Blink
-   Turns on an LED on for one second, then off for one second, repeatedly.
+   Hello, World! example
+   June 11, 2015
+   Copyright (C) 2015 David Martinez
+   All rights reserved.
+   This code is the most basic barebones code for writing a program for Arduboy.
 
-   This example code is in the public domain.
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
  */
 
-void setup () {
-  // initialize the digital pin as an output.
-  // Pin 13 has an LED connected on most Arduino boards:
-  pinMode(13, OUTPUT);
+#include "Arduboy.h"
+
+// make an instance of arduboy used for many functions
+Arduboy arduboy;
+
+
+// This function runs once in your game.
+// use it for anything that needs to be set only once in your game.
+void setup() {
+  // initiate arduboy instance
+  arduboy.begin();
+
+  // here we set the framerate to 15, we do not need to run at
+  // default 60 and it saves us battery life
+  arduboy.setFrameRate(15);
 }
 
-void loop () {
-  digitalWrite(13, HIGH);   // set the LED on
-  delay(2000);              // wait for a second
-  digitalWrite(13, LOW);    // set the LED off
-  delay(1000);              // wait for a second
+
+// our main game loop, this runs once every cycle/frame.
+// this is where our game logic goes.
+void loop() {
+  // pause render until it's time for the next frame
+  if (!(arduboy.nextFrame()))
+    return;
+
+  // first we clear our screen to black
+  arduboy.clear();
+
+  // we set our cursor 5 pixels to the right and 10 down from the top
+  // (positions start at 0, 0) 
+  arduboy.setCursor(4, 9);
+
+  // then we print to screen what is in the Quotation marks ""
+  arduboy.print(F("Hello, world!"));
+
+  // then we finaly we tell the arduboy to display what we just wrote to the display
+  arduboy.display();
 }
