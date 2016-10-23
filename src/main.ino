28d6bbebce187dd9797ddc0a0d063878e787cb22
Eric Betts <bettse@fastmail.fm>
2016-10-22 18:04:54 -0700

    Dedicate certain lines of UI to IO display

--- src/main.ino
+++ src/main.ino
@@ -1,26 +1,18 @@
-/*
-   Hello, World! example
-   June 11, 2015
-   Copyright (C) 2015 David Martinez
-   All rights reserved.
-   This code is the most basic barebones code for writing a program for Arduboy.
-
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.
- */
-
 #include "Arduboy.h"
 #include "RawHID.h"
 #include "VirtualPortal.h"
 
 #define HID_MAX_LENGTH 32
 
+#define LINE_HEIGHT 8
+
+#define LINE_INCOMING 0 * LINE_HEIGHT
+#define LINE_OUTGOING 1 * LINE_HEIGHT
+#define LINE_LOADS 2 * LINE_HEIGHT
+
 // make an instance of arduboy used for many functions
 Arduboy arduboy;
 uint8_t rawhidData[HID_MAX_LENGTH];
-uint8_t featureReport[HID_MAX_LENGTH] = {0};
 
 VirtualPortal vp = VirtualPortal();
 long previousMillis = 0;
@@ -30,9 +22,10 @@ int libraryId = 0; //Token being displayed
 Token *next = NULL;
 unsigned int token_count = 1;
 
+int blockNum = 0;
+
 void setup() {
   RawHID.begin(rawhidData, sizeof(rawhidData));
-  RawHID.setFeatureReport(featureReport, sizeof(featureReport));
 
   // initiate arduboy instance
   arduboy.beginNoLogo();
@@ -57,20 +50,22 @@ void loop() {
       uint8_t response[HID_MAX_LENGTH] = {0};
       uint8_t len = vp.status(response);
       if(len) {
-        arduboy.print("->");
+        arduboy.setCursor(0, LINE_OUTGOING);
+        arduboy.print("<-");
         arduboy.write((char)response[0]);
         for (int i = 1; i < len; i++) {
           printHex(response[i]);
         }
-        arduboy.println("");
         RawHID.write(response, HID_MAX_LENGTH);
       }
     }
+
   }
 
   auto bytesAvailable = RawHID.available();
   uint8_t incoming[HID_MAX_LENGTH] = {0};
   if (bytesAvailable) {
+    arduboy.setCursor(0, LINE_INCOMING);
     arduboy.print("=>");
     for (int i = 0; i < bytesAvailable; i++) {
       incoming[i] = RawHID.read();
@@ -81,29 +76,28 @@ void loop() {
     uint8_t response[HID_MAX_LENGTH] = {0};
     uint8_t len = vp.respondTo(incoming, response);
     if(len) {
-      arduboy.write('/');
+      arduboy.setCursor(0, LINE_OUTGOING);
+      arduboy.print("<=");
       arduboy.write((char)response[0]);
       for (int i = 1; i < 6; i++) {
         printHex(response[i]);
       }
       RawHID.write(response, HID_MAX_LENGTH);
     }
-    arduboy.println(".");
   }
 
   if (arduboy.pressed(A_BUTTON)) {
     arduboy.clear();
-    arduboy.setCursor(0, 0);
   } else if (arduboy.pressed(B_BUTTON)) {
     if(next == NULL) {
       next = new Token(0);
-      arduboy.println(next->getName());
+      arduboy.setCursor(0, LINE_LOADS);
+      arduboy.print("Loaded ");
+      arduboy.print(next->getName());
     }
   } else if (arduboy.pressed(UP_BUTTON)) {
-    //libraryId++;
   } else if (arduboy.pressed(RIGHT_BUTTON)) {
   } else if (arduboy.pressed(DOWN_BUTTON)) {
-    //libraryId--;
   } else if (arduboy.pressed(LEFT_BUTTON)) {
   }
   libraryId = positive_modulo(libraryId, token_count);
@@ -119,4 +113,6 @@ void printHex(int num) {
 inline int positive_modulo(int i, int n) {
   return (i % n + n) % n;
 }
+
+
 
